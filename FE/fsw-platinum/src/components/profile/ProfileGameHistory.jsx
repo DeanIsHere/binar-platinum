e87f02ae64faf9db6aa6a66af5c1c699736f6a17
argikurnia <argikurnia2@gmail.com>
2022-11-01 08:43:39 +0700

    Create Profile Page

---
+++ FE/fsw-platinum/src/components/profile/ProfileGameHistory.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./ProfileGameHistory.css";
+
+const ProfileGameHistory = () => {
+  return (
+    <section class="section-profile__game--history">
+      <Card
+        style={{
+          backgroundColor: "#3B3838",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25);",
+          height: "100vh",
+        }}
+      >
+        {/* ProfileGameHistory Top */}
+        <div style={{ backgroundColor: "#464343" }}>
+          <Card.Header className="profile-game__history--header">
+            GAME HISTORY
+          </Card.Header>
+        </div>
+
+        {/* ProfileGameHistory Bottom */}
+        <Card.Body>{/* Game History */}</Card.Body>
+      </Card>
+    </section>
+  );
+};
+
+export default ProfileGameHistory;
---
+++ FE/fsw-platinum/src/pages/profile/Profile.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Footer from "../../components/layout/footer/Footer";
+import Navbar from "../../components/layout/nav/Navbar";
+import ProfileGameHistory from "../../components/profile/ProfileGameHistory";
+import ProfileHeader from "../../components/profile/ProfileHeader";
+const Profile = () => {
+  const [userData, setUserData] = useState({
+    id: "1",
+    playerName: "ELON MUSK22",
+    username: "ELON MUSK",
+    totalGame: "12",
+    totalPoint: "125",
+    playerRank: "1.342",
+  });
+  const { id: playerId } = useParams();
+
+  useEffect(() => {
+    // Fetch User By Id
+  }, []);
+
+  return (
+    <>
+      <div style={{ overflow: "hidden" }}>
+        <Navbar bgColor="#4A4A5C" />
+        <main
+          className="min-vh-100"
+          style={{ backgroundColor: "#201C1C", paddingBottom: "2.875rem" }}
+        >
+          {/* Section Top*/}
+          <article
+            style={{ maxWidth: "1024px", margin: "auto", padding: "9rem 0rem" }}
+          >
+            <Container>
+              <ProfileHeader user={userData} />
+            </Container>
+          </article>
+
+          {/* Section Bottom */}
+          <article style={{ maxWidth: "1024px", margin: "auto" }}>
+            <Container>
+              <ProfileGameHistory user={userData} />
+            </Container>
+          </article>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Profile;
