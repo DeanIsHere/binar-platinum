c921ec37684c77cd84139f84ad16a8ffa535a6ab
argikurnia <argikurnia2@gmail.com>
2022-11-06 22:00:29 +0700

    style:add comma after email :arts:

--- FE/fsw-platinum/src/action/fb_database.js
+++ FE/fsw-platinum/src/action/fb_database.js
@@ -1,64 +1,64 @@
 import { set, ref, push, onValue, update } from "firebase/database";
-import { database} from "../config/firebase";
+import { database } from "../config/firebase";
 
-const db = database
+const db = database;
 
 //write biodata
 
-export const CreateUser = (name,username,email) =>{
-    const dbRef = ref(db,'game_user')
-    const data = {
-        name,
-        username,
-        email //nanti ambil dari auth via decode jwt
-        total_score:0,
-        city: null,
-        social_media: null,
-        profile_picture: null
-    }
-    push(dbRef, data)
-}
+export const CreateUser = (name, username, email) => {
+  const dbRef = ref(db, "game_user");
+  const data = {
+    name,
+    username,
+    email, //nanti ambil dari auth via decode jwt
+    total_score: 0,
+    city: null,
+    social_media: null,
+    profile_picture: null,
+  };
+  push(dbRef, data);
+};
 
 //read one biodata
 export const getUserById = (id) => {
-    return new Promise((resolve, reject) => {
-      const dbRef = ref(db, `game_user/${id}`)
-      onValue(dbRef, (data) => {
-        const value = data.val()
-        resolve(value)
-      })
-    })
-  }
+  return new Promise((resolve, reject) => {
+    const dbRef = ref(db, `game_user/${id}`);
+    onValue(dbRef, (data) => {
+      const value = data.val();
+      resolve(value);
+    });
+  });
+};
 
 //edit profile
-export const updateProfile = (id, name,username, city, social_media) => {
-    const dbRef = ref(db, `game_user/${id}`)
-    const data = {
-        name,
-        username,
-        city,
-        social_media
-    }
-    update(dbRef, data)
-  }
+export const updateProfile = (id, name, username, city, social_media) => {
+  const dbRef = ref(db, `game_user/${id}`);
+  const data = {
+    name,
+    username,
+    city,
+    social_media,
+  };
+  update(dbRef, data);
+};
 
 //update score
 export const updateScore = (id, total_score) => {
-    const dbRef = ref(db, `game_user/${id}`)
-    const data = {
-        total_score
-    }
-    update(dbRef, data)
-  }
+  const dbRef = ref(db, `game_user/${id}`);
+  const data = {
+    total_score,
+  };
+  update(dbRef, data);
+};
 
 //update profile photo
 export const updateProfileImg = (id, profile_picture) => {
-    const dbRef = ref(db, `game_user/${id}`)
-    const data = {
-        profile_picture
-    }
-    update(dbRef, data)
-  }
+  const dbRef = ref(db, `game_user/${id}`);
+  const data = {
+    profile_picture,
+  };
+  update(dbRef, data);
+};
 
 //total point
 
@@ -66,4 +66,4 @@ export const updateProfileImg = (id, profile_picture) => {
 
 //leaderboard pergame
 
-//jumlah user yang bermain pergame 
+//jumlah user yang bermain pergame
