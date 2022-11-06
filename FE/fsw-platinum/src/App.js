071b44aabd4db607b5f921e8cff6607c9b07f43d
argikurnia <argikurnia2@gmail.com>
2022-11-06 22:22:46 +0700

    feat:add setup react-toastify :sparkles:

--- FE/fsw-platinum/src/App.js
+++ FE/fsw-platinum/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { authFirebase } from "./config/firebase";
+import { ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Home from "./pages/home";
 import GameSpaceWar from "./pages/games/space_war";
@@ -34,17 +36,20 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game/spacewar" element={<GameSpaceWar />} />
-        <Route path="/game/game_rps" element={<GameRPS />} />
-        <Route path="/profile/:id" element={<Profile />} />
-        <Route path="/detail/:game" element={<GameDetail />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/edit_profile/:id" element={<EditProfile />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game/spacewar" element={<GameSpaceWar />} />
+          <Route path="/game/game_rps" element={<GameRPS />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/detail/:game" element={<GameDetail />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/edit_profile/:id" element={<EditProfile />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
