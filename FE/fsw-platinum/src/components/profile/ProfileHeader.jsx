1e785d20829557434ba9f444fab5cf8dc07d011c
fauzan.gahara <fauzanfg20@gmail.com>
2022-11-09 20:45:26 +0700

    benerin logout

--- FE/fsw-platinum/src/components/profile/ProfileHeader.jsx
+++ FE/fsw-platinum/src/components/profile/ProfileHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
 import "./ProfileHeader.css";
 
 const ProfileHeader = ({ user, totalGame, totalPoint, playerRankByUser }) => {
